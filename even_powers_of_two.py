c3ed1858664ad6cebcc0c60537f17f93e0a329b9
Ivan Stoyanov <ivan.st.workmail98@gmail.com>
2022-08-14 23:55:10 +0300

    f

--- even_powers_of_two.py
+++
@@ -1,4 +0,0 @@
-powered_number = int(input())
-
-for number in range(0, powered_number + 1, 2):
-    print(2 ** number)
--- sum_of_a_beach.py
+++
@@ -1,9 +0,0 @@
-string = input().lower()
-counter = 0
-
-sand = string.count("sand")
-water = string.count("water")
-fish = string.count("fish")
-sun = string.count("sun")
-counter = counter + sand + water + fish + sun
-print(counter)
